14c26c62af9e3890e4f9224f244cd571edacbcd2
belse-de <belse.de@gmail.com>
2017-12-20 00:13:35 +0100

    started c++ed tut10

--- LazyFooSDL2Tutorial/10_color_keying/10_color_keying.cpp
+++ LazyFooSDL2Tutorial/10_color_keying/10_color_keying.cpp
@@ -6,6 +6,12 @@ and may not be redistributed without written permission.*/
 #include <SDL_image.h>
 #include <stdio.h>
 #include <string>
+#include <chrono>
+#include <SDL2.hpp>
+#include <SDL2Image.hpp>
+#include <iostream>
+#include <Window.hpp>
+#include <Image.hpp>
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
@@ -42,26 +48,6 @@ class LTexture
 		int mWidth;
 		int mHeight;
 };
-
-//Starts up SDL and creates window
-bool init();
-
-//Loads media
-bool loadMedia();
-
-//Frees media and shuts down SDL
-void close();
-
-//The window we'll be rendering to
-SDL_Window* gWindow = NULL;
-
-//The window renderer
-SDL_Renderer* gRenderer = NULL;
-
-//Scene textures
-LTexture gFooTexture;
-LTexture gBackgroundTexture;
-
 
 LTexture::LTexture()
 {
@@ -147,101 +133,31 @@ int LTexture::getHeight()
 	return mHeight;
 }
 
-bool init()
-{
-	//Initialization flag
-	bool success = true;
-
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
-		success = false;
-	}
-	else
-	{
-		//Set texture filtering to linear
-		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
-		{
-			printf( "Warning: Linear texture filtering not enabled!" );
-		}
-
-		//Create window
-		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
-		{
-			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
-			success = false;
-		}
-		else
-		{
-			//Create renderer for window
-			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
-			if( gRenderer == NULL )
-			{
-				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
-				success = false;
-			}
-			else
-			{
-				//Initialize renderer color
-				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-
-				//Initialize PNG loading
-				int imgFlags = IMG_INIT_PNG;
-				if( !( IMG_Init( imgFlags ) & imgFlags ) )
-				{
-					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
-					success = false;
-				}
-			}
-		}
-	}
-
-	return success;
-}
-
-bool loadMedia()
-{
-	//Loading success flag
-	bool success = true;
-
-	//Load Foo' texture
-	if( !gFooTexture.loadFromFile( "10_color_keying/foo.png" ) )
-	{
-		printf( "Failed to load Foo' texture image!\n" );
-		success = false;
-	}
-	
-	//Load background texture
-	if( !gBackgroundTexture.loadFromFile( "10_color_keying/background.png" ) )
-	{
-		printf( "Failed to load background texture image!\n" );
-		success = false;
-	}
-
-	return success;
-}
-
-void close()
-{
-	//Free loaded images
-	gFooTexture.free();
-	gBackgroundTexture.free();
-
-	//Destroy window	
-	SDL_DestroyRenderer( gRenderer );
-	SDL_DestroyWindow( gWindow );
-	gWindow = NULL;
-	gRenderer = NULL;
-
-	//Quit SDL subsystems
-	IMG_Quit();
-	SDL_Quit();
-}
-
 int main( int argc, char* args[] )
 {
+	using namespace std::chrono_literals;
+	//Start up SDL and create window
+	//Initialize SDL
+	SDL2pp::SDL2 sdl;
+	SDL2pp::Img::SDL2Image img;
+
+	if (not sdl.setHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
+		std::clog << "Warning: Linear texture filtering not enabled!" << std::endl;
+	//The window we'll be rendering to
+	SDL2pp::Window window("SDL Tutorial 09", SCREEN_WIDTH, SCREEN_HEIGHT);
+	//The surface contained by the window
+	//Get window surface
+	SDL2pp::Surface screen = window.getSurface();
+	SDL2pp::Renderer renderer = window.createRenderer(1, SDL_RENDERER_ACCELERATED);
+	//Initialize renderer color
+	renderer.setDrawColor(0xFF, 0xFF, 0xFF, 0xFF);
+
+	SDL2pp::Img::Image background_texture_surface("10_color_keying/background.png");
+	SDL2pp::Texture background_texture = renderer.createTexture(background_texture_surface);
+	SDL2pp::Img::Image foo_texture_surface("10_color_keying/foo.png");
+	SDL2pp::Texture foo_texture = renderer.createTexture(background_texture_surface);
+
+
 	//Start up SDL and create window
 	if( !init() )
 	{
@@ -276,8 +192,8 @@ int main( int argc, char* args[] )
 				}
 
 				//Clear screen
-				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-				SDL_RenderClear( gRenderer );
+				renderer.setDrawColor(0xFF, 0xFF, 0xFF, 0xFF );
+				renderer.clear();
 
 				//Render background texture to screen
 				gBackgroundTexture.render( 0, 0 );
@@ -286,7 +202,7 @@ int main( int argc, char* args[] )
 				gFooTexture.render( 240, 190 );
 
 				//Update screen
-				SDL_RenderPresent( gRenderer );
+				renderer.present();
 			}
 		}
 	}
