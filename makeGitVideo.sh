a7d6dffc461afdce2532cb3add4e595852ad245b
belse-de <belse.de@gmail.com>
2018-03-20 20:41:51 +0100

    added shellscript for generating fancy video from git history

---
+++ makeGitVideo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+RES="640x480"
+RATE="60"
+PROJECT="sdl2pp"
+
+GS_TITLE="--title $PROJECT"
+#GS_LOGO="--logo logo.png"
+GS_LOGO=""
+#GS_DATE='--date-format "%FT%TZ"'
+GS_DATE="--date-format %F"
+#GS_HIDE="--hide bloom,date,dirnames,files,filenames,mouse,progress,tree,users,usernames"
+GS_HIDE=""
+GS="gource $GS_TITLE $GS_LOGO $GS_DATE $GS_HIDE"
+#--seconds-per-day 1
+
+# webm
+$GS -$RES -o - | ffmpeg -y -r $RATE -f image2pipe -vcodec ppm -i - -vcodec libvpx -b 10000K ../$PROJECT-gource.webm
+
+#mp4
+#gource -$RES -o - | ffmpeg -y -r $RATE -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 ../$PROJECT-gource.mp4
+
+#gource -$RES -o ../$PROJECT-gource.ppm
+#ffmpeg -y -r $RATE -f image2pipe -vcodec ppm -i../$PROJECT-gource.ppm -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 gource.mp4
+exit
+gource --date-format %F -1024x768 --seconds-per-day 1 --auto-skip-seconds 1 --hide date,dirnames,filenames,mouse,progress -r 30 -o - | ffmpeg -y -r 30 -f image2pipe -vcodec ppm -i - -vcodec libvpx -b:v 100000K -threads 3 ../hmm_toolbox-gource-100000K.webm
+
+gource --date-format %F -1024x768 --seconds-per-day 1 --auto-skip-seconds 1 --hide date,dirnames,filenames,mouse,progress -r 30 -o - | ffmpeg -y -r 30 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 3 -bf 0 ../hmm_toolbox-gource.mp4
