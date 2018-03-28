7c0097e821ec6af04e03baf9535840120806dd4b
belse-de <belse.de@gmail.com>
2018-03-28 02:13:00 +0200

    sorted some sprites

---
+++ main_svg_playground.cpp
@@ -0,0 +1,12 @@
+//
+// Created by belse on 26.03.18.
+//
+
+#include <iostream>
+
+#include <librsvg/rsvg.h>
+#include <librsvg/rsvg-cairo.h>
+
+int main(int argC, char* argV[]){
+    std::cout << "Hello World!" << std::endl;
+}
