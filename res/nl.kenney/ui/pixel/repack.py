5bb651c51333474496acdf28ea021672755e8d40
belse-de <belse.de@gmail.com>
2018-04-09 00:52:31 +0200

    started trying to copy common files into every variation

--- res/nl.kenney/ui/pixel/repack.py
+++ res/nl.kenney/ui/pixel/repack.py
@@ -6,48 +6,85 @@ Created on Tue Mar 29 10:50:37 2018
 @author: belse
 """
 
-prefixes = [
-        'ancient.white',
-        'ancient.brown',
-        'ancient.grey',
-        'ancient.tan',
-        'color.grey',
-        'color.red',
-        'color.green',
-        'color.blue',
-        'color.yellow',
-        'space',
+commenPrefixes = [
+        'ancient',
+        'color',
+        #'space'
         ]
+
+prefixes = {
+        'ancient': [
+            'ancient.white',
+            'ancient.brown',
+            'ancient.grey',
+            'ancient.tan',
+            ],
+        'color':[
+            'color.grey',
+            'color.red',
+            'color.green',
+            'color.blue',
+            'color.yellow',
+            ]
+        #'space':[
+        #        ]
+        }
 
 # execute only if run as a script
 if __name__ == "__main__":
     import os, glob, shutil
     pngsGlobStr = './pack/**/'
-    for pre in prefixes:
-        globStr = pngsGlobStr + pre + '.*.png'
-        print(globStr)
-        pngsGlob = glob.glob(globStr, recursive=True)
-        pngsGlob.sort()
-        oldDir = ''
-        newDir = ''
-        for png in pngsGlob:
-            pngDir  = os.path.dirname(png)
-            pngName = os.path.basename(png)
+    
+    for com in commenPrefixes:
+        comGlobStr = pngsGlobStr + com + '.*.png'
+        comPngsGlob = glob.glob(comGlobStr, recursive=True)
+        comPngsGlob.sort()
+    
+        for pre in prefixes[com]:
+            globStr = pngsGlobStr + pre + '.*.png'
+            #print(globStr)
+            pngsGlob = glob.glob(globStr, recursive=True)
+            pngsGlob.sort()
             
-            newName = pngName[len(pre)+1:]
+            oldDir = ''
+            newDir = ''
+            for png in pngsGlob:
+                comPngsGlob.remove(png)
+                continue
             
-            if oldDir != pngDir:
-                newDir = pngDir.replace('pack', 'packed/' + pre, 1)
-                print(pngDir, '->', )
-                oldDir = pngDir
-                os.makedirs(newDir)
+                pngDir  = os.path.dirname(png)
+                pngName = os.path.basename(png)         
+                newName = pngName[len(pre)+1:]
                 
-            print('  ', pngName, '->', newName)
-            print('     ', os.path.join(newDir,newName))
-            shutil.copy(png, os.path.join(newDir,newName))
-            #shutil.move(png, os.path.join(newDir,newName))
-            
-            
+                if oldDir != pngDir:
+                    newDir = pngDir.replace('pack', 'packed/' + pre, 1)
+                    #print(pngDir, '->', newDir)
+                    oldDir = pngDir
+                    if not os.path.exists(newDir):
+                        os.makedirs(newDir)
+                    
+                #print('  ', pngName, '->', newName)
+                #print('     ', os.path.join(newDir,newName))
+                shutil.copy(png, os.path.join(newDir,newName))
+                #shutil.move(png, os.path.join(newDir,newName))
+             
+            oldDir = ''
+            newDir = ''
+            for png in comPngsGlob:
+                pngDir  = os.path.dirname(png)
+                pngName = os.path.basename(png) 
+                newName = pngName[len(com)+1:]
+                
+                for pre in prefixes[com]:        
+                    if oldDir != pngDir:
+                        newDir = pngDir.replace('pack', 'packed/' + pre, 1)
+                        #print(pngDir, '->', newDir)
+                        oldDir = pngDir
+                        if not os.path.exists(newDir):
+                            os.makedirs(newDir)
+                    
+                print('   ', os.path.join(newDir,newName))
+                #shutil.copy(png, os.path.join(newDir,newName))
             #pngNewName = pngName[10:]
             #print(pngName, '->', pngNewName )
             #newPng = os.path.join(pngDir, pngNewName)
---
+++ src/stdLib_helper.hpp
@@ -0,0 +1,8 @@
+//
+// Created by belse on 09.04.18.
+//
+
+#ifndef SDL2PP_STDLIB_HELPER_HPP
+#define SDL2PP_STDLIB_HELPER_HPP
+
+#endif //SDL2PP_STDLIB_HELPER_HPP
