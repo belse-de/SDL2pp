dbdf8441931d17e664a73815c4700e052c441fa0
belse-de <belse.de@gmail.com>
2018-04-11 15:23:29 +0200

    fixed repack

--- res/nl.kenney/ui/pixel/repack.py
+++ res/nl.kenney/ui/pixel/repack.py
@@ -6,10 +6,26 @@ Created on Tue Mar 29 10:50:37 2018
 @author: belse
 """
 
+dryRun = True
+verbose = False
+fileMove = True
+
+fileMode = 'dry'
+if dryRun:
+    if verbose:
+        fileMode = 'dry_verbose'
+    else:
+        fileMode = 'dry'
+elif fileMove:
+    fileMode = 'mv'
+else:
+    fileMode = 'cp'
+
+
 commenPrefixes = [
         'ancient',
         'color',
-        #'space'
+        'space'
         ]
 
 prefixes = {
@@ -25,10 +41,40 @@ prefixes = {
             'color.green',
             'color.blue',
             'color.yellow',
-            ]
-        #'space':[
-        #        ]
+            ],
+        'space':[
+                ]
         }
+
+
+def globRecSorted(globStr):
+    files = glob.glob(globStr, recursive=True)
+    files.sort()
+    return files
+
+def fileCpMv(src, dst, mode='dry_verbose'):
+    if mode == 'dry':
+        pass
+    elif mode == 'dry_verbose':
+        print(src, '->', dst)
+    else:
+        dstDir = os.path.dirname(png)
+        if not os.path.exists(dstDir):
+            os.makedirs(dstDir)
+                        
+        if mode == 'cp' or mode == 'copy':
+             shutil.copy(src, dst)
+        elif mode == 'mv' or mode == 'move':
+            shutil.move(src, dst)
+        else:
+            assert False, 'Not valide mode.'
+
+def recDictTraversal(d, handle=None):
+  for k, v in d.items():
+    if isinstance(v, dict):
+      recDictTraversal(v, handle)
+    else:
+      handle(k, v)
 
 # execute only if run as a script
 if __name__ == "__main__":
@@ -36,57 +82,40 @@ if __name__ == "__main__":
     pngsGlobStr = './pack/**/'
     
     for com in commenPrefixes:
-        comGlobStr = pngsGlobStr + com + '.*.png'
-        comPngsGlob = glob.glob(comGlobStr, recursive=True)
-        comPngsGlob.sort()
-    
+        comPngsGlob = globRecSorted(pngsGlobStr + com + '.*.png')
+        print(com, len(comPngsGlob))
+        
+        packedPngs = []
         for pre in prefixes[com]:
-            globStr = pngsGlobStr + pre + '.*.png'
-            #print(globStr)
-            pngsGlob = glob.glob(globStr, recursive=True)
-            pngsGlob.sort()
-            
-            oldDir = ''
-            newDir = ''
+            pngsGlob = globRecSorted(pngsGlobStr + pre + '.*.png')
+            print(pre, len(pngsGlob))  
+
             for png in pngsGlob:
                 comPngsGlob.remove(png)
-                continue
-            
+    
                 pngDir  = os.path.dirname(png)
+                newDir = pngDir.replace('pack', 'packed/' + pre, 1)  
                 pngName = os.path.basename(png)         
                 newName = pngName[len(pre)+1:]
-                
-                if oldDir != pngDir:
-                    newDir = pngDir.replace('pack', 'packed/' + pre, 1)
-                    #print(pngDir, '->', newDir)
-                    oldDir = pngDir
-                    if not os.path.exists(newDir):
-                        os.makedirs(newDir)
+                newPng = os.path.join(newDir,newName)
                     
-                #print('  ', pngName, '->', newName)
-                #print('     ', os.path.join(newDir,newName))
-                shutil.copy(png, os.path.join(newDir,newName))
-                #shutil.move(png, os.path.join(newDir,newName))
-             
-            oldDir = ''
-            newDir = ''
-            for png in comPngsGlob:
-                pngDir  = os.path.dirname(png)
-                pngName = os.path.basename(png) 
-                newName = pngName[len(com)+1:]
+                fileCpMv(src=png, dst=newPng, mode=fileMode)            
                 
+            print('   ', com, len(comPngsGlob), pre, len(pngsGlob))  
+            
+        for png in comPngsGlob:
+            pngDir  = os.path.dirname(png)
+            pngName = os.path.basename(png) 
+            newName = pngName[len(com)+1:]
+            newFile = ''
+            
+            if len(prefixes[com]) > 0:
                 for pre in prefixes[com]:        
-                    if oldDir != pngDir:
-                        newDir = pngDir.replace('pack', 'packed/' + pre, 1)
-                        #print(pngDir, '->', newDir)
-                        oldDir = pngDir
-                        if not os.path.exists(newDir):
-                            os.makedirs(newDir)
-                    
-                print('   ', os.path.join(newDir,newName))
-                #shutil.copy(png, os.path.join(newDir,newName))
-            #pngNewName = pngName[10:]
-            #print(pngName, '->', pngNewName )
-            #newPng = os.path.join(pngDir, pngNewName)
-            #print(png, '->', newPng )
-            #os.rename(png, newPng)
+                    newDir = pngDir.replace('pack', 'packed/' + pre, 1)
+                    newPng = os.path.join(newDir,newName)
+                    packedPngs.append(newPng)
+                    fileCpMv(src=png, dst=newPng, mode=fileMode)  
+            else:
+                newDir = pngDir.replace('pack', 'packed/' + com, 1)
+                newPng = os.path.join(newDir,newName)
+                packedPngs.append(newPng)
